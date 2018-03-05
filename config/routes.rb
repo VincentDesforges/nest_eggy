d11699877dcf823e9acdb1bc0cf504781fc1f68c
Johann Goedhart <johann@goedhart.biz>
2018-03-05 17:25:13 +0000

    added routes for th, svg and brd of spendings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'test_components', to: 'pages#test_components'
+  get "users/:id/transaction_history", as: "th"
+  get "users/:id/savings", as: "th"
+  get "users/:id/breakdown", as: "th"
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
