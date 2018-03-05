6685bd3fb89aac9e58d6824466932a3cee416328
Johann Goedhart <johann@goedhart.biz>
2018-03-05 17:29:39 +0000

    added controller actions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'test_components', to: 'pages#test_components'
-  get "users/:id/transaction_history", as: "th"
-  get "users/:id/savings", as: "th"
-  get "users/:id/breakdown", as: "th"
+  get "users/:id/transaction_history", as: "th", to: 'pages#transaction_history'
+  get "users/:id/savings", as: "svg", to: 'pages#savings'
+  get "users/:id/breakdown", as: "brd", to: 'pages#breakdown'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
