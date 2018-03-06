747843f1454379d541681f7f299fbbef78b06060
Vincent Desforges <desforges.vincent@gmail.com>
2018-03-06 17:01:26 +0000

    put all the api methods in a module and called create api on sign up

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -1,45 +1,32 @@
+require 'api_calls'
+
 class BankAccountsController < ApplicationController
+
   def new
-    base_uri = 'https://sync.bankin.com/v2/'
-    bankin_version ="2016-01-18"
     @user = current_user
-    simulation_bank_id = 408
-    email = "john.doe@email.com"
-    password = "a!StrongP455word"
-    client_id = ENV["CLIENT_ID"]
-    client_secret = ENV["CLIENT_SECRET"]
-    bearer_token = 'cd48224d8d1294c97f0eaf5b4d4c80519c79e47e-6f1f4401-7b39-4bf5-9a6c-ea82eaa4b221'
-    api_account_id = 10648786
-    until_date = '2018-03-06'
-    redirect_url = 'http://localhost:3000/test_components'
+    bearer_token = '90590727414157c206a6ed7d4d3b3908918cbc0f-357b7c9f-9114-40fa-bf1c-133b332949cd'
+    api_account_id = 10657952
 
-    api_options = {
-      base_uri: base_uri,
-      bankin_version: bankin_version,
-      bank_id: simulation_bank_id,
-      email: email,
-      password: password,
-      client_id: client_id,
-      client_secret: client_secret,
-      has_bearer: false,
-      bearer_token: bearer_token,
-      api_account_id: api_account_id,
-      until_date: until_date,
-      redirect_url: redirect_url
-    }
+    # email = "john.doe@email.com"
+    # password = "a!StrongP455word"
+    # @user_details = {
+    #   email: email,
+    #   password: password
+    # }
 
-    # @response = list_banks(api_options)
-    # @response = single_bank(api_options)
-    # @response = list_users(api_options)
-    # @response = create_user(api_options)
-    # @response = authenticate_user(api_options)
-    # @response = list_accounts(api_options)
-    # @response = single_account(api_options)
-    # add_item(api_options)
-    # @response = list_items(api_options)
-    @response = list_tansactions(api_options)
 
-    # @response = clear_database(api_options) # care lose all data!!!
+    # @response = ApiCalls::RequestMethods.list_banks
+    # @response = ApiCalls::RequestMethods.single_bank
+    @response = ApiCalls::RequestMethods.list_users
+    # @response = ApiCalls::RequestMethods.create_user(@user_details)
+    # @response = ApiCalls::RequestMethods.authenticate_user(@user_details)
+    # @response = ApiCalls::RequestMethods.list_accounts(bearer_token)
+    # @response = ApiCalls::RequestMethods.single_account(bearer_token, api_account_id)
+    # redirect_to ApiCalls::RequestMethods.add_item(bearer_token)
+    # @response = ApiCalls::RequestMethods.list_items(bearer_token)
+    # @response = ApiCalls::RequestMethods.list_tansactions(bearer_token)
+
+    # @response = ApiCalls::RequestMethods.clear_database # care lose all data!!!
   end
 
 
@@ -48,91 +35,6 @@ class BankAccountsController < ApplicationController
   private
 
 
-  def api_get_request(uri, api_options)
-    req = Net::HTTP::Get.new(uri)
-    req['Authorization'] = "Bearer #{api_options[:bearer_token]}" if api_options[:has_bearer]
-    req['Bankin-Version'] = api_options[:bankin_version]
-
-    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
-      http.request(req)
-    }
-    JSON.parse(res.body)
-  end
-
-  def api_post_request(uri, api_options)
-    req = Net::HTTP::Post.new(uri)
-    req['Authorization'] = "Bearer #{api_options[:bearer_token]}" if api_options[:has_bearer]
-    req['Bankin-Version'] = api_options[:bankin_version]
-
-    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
-      http.request(req)
-    }
-    JSON.parse(res.body)
-  end
-
-  def list_banks(api_options)
-    uri = URI("#{api_options[:base_uri]}banks?client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}")
-    api_get_request(uri, api_options)
-  end
-
-  def single_bank(api_options)
-    uri = URI("#{api_options[:base_uri]}banks/#{api_options[:bank_id]}?client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}")
-    api_get_request(uri, api_options)
-  end
-
-  def list_users(api_options)
-    uri = URI("#{api_options[:base_uri]}users?limit=100&client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}")
-    api_get_request(uri, api_options)
-  end
-
-  def create_user(api_options)
-    uri = URI("#{api_options[:base_uri]}users?email=#{api_options[:email]}&password=#{api_options[:password]}&client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}")
-    api_post_request(uri, api_options)
-  end
-
-  def authenticate_user(api_options)
-    uri = URI("#{api_options[:base_uri]}authenticate?email=#{api_options[:email]}&password=#{api_options[:password]}&client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}")
-    api_post_request(uri, api_options)
-  end
-
-  def list_accounts(api_options)
-    uri = URI("#{api_options[:base_uri]}accounts?limit=10&client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}")
-    api_options[:has_bearer] = true
-    api_get_request(uri, api_options)
-  end
-
-  def single_account(api_options)
-    uri = URI("#{api_options[:base_uri]}accounts/#{api_options[:api_account_id]}?client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}")
-    api_options[:has_bearer] = true
-    api_get_request(uri, api_options)
-  end
-
-  def add_item(api_options)
-    url = "#{api_options[:base_uri]}items/connect?bank_id=#{api_options[:bank_id]}&client_id=#{api_options[:client_id]}&access_token=#{api_options[:bearer_token]}&redirect_url=#{api_options[:redirect_url]}"
-    # !!!!!!!
-    redirect_to url
-  end
-
-  def list_items(api_options)
-    uri = URI("#{api_options[:base_uri]}items?client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}&limit=100")
-    api_options[:has_bearer] = true
-    api_get_request(uri, api_options)
-  end
-
-  def list_tansactions(api_options)
-    uri = URI("#{api_options[:base_uri]}transactions?limit=50&until=#{api_options[:until_date]}&client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}")
-    api_options[:has_bearer] = true
-    api_get_request(uri, api_options)
-  end
-
-  def clear_database(api_options)
-    uri = URI("#{api_options[:base_uri]}users?client_id=#{api_options[:client_id]}&client_secret=#{api_options[:client_secret]}")
-    req = Net::HTTP::Delete.new(uri)
-    req['Bankin-Version'] = api_options[:bankin_version]
-    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) {|http|
-      http.request(req)
-    }
-  end
 end
 
 
