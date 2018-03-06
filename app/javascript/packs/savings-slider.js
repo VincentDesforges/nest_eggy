fec7c4fb6c7277bee67e85dab9d9ffbc664f0d19
Adam Smith <adamsmith098@gmail.com>
2018-03-06 18:51:54 +0000

    savings page draft

---
+++ app/javascript/packs/savings-slider.js
@@ -0,0 +1,40 @@
+const saving = document.getElementById('savings-slider');
+const savingsComment = document.getElementById('savings-comment');
+const savingsValue = document.getElementById('savings-value');
+
+saving.addEventListener("change", (event) => {
+  const val = event.target.value
+  savingsValue.innerText = val
+
+  if (val > 250) {
+    savingsComment.innerText = "That's the attitude!"
+  } else if ( val < 250 ) {
+    savingsComment.innerText = "Come on, make an effort!"
+  }
+});
+
+const percent = document.getElementById('return-slider');
+const returnComment = document.getElementById('return-comment');
+const returnValue = document.getElementById('return-value');
+
+percent.addEventListener("change", (event) => {
+  const value = event.target.value
+  returnValue.innerText = value
+
+  if (value > 8) {
+    returnComment.innerText = "Aggressive portfolio - 100% Equities"
+  } else if ( value > 6 ) {
+    returnComment.innerText = "Moderate portfolio - 60% Equities 40% Bonds"
+  } else if ( value > 4 ) {
+    returnComment.innerText = "Conservative portfolio - 40% Equitiess 40% Bonds"
+  } else if ( value > 2 ) {
+    returnComment.innerText = "Fixed Income Portfolio - 100% bonds"
+  } else if ( value <= 2 ) {
+    returnComment.innerText = "Risk Free Deposit - Money Market Return"
+  }
+
+
+
+
+
+});
