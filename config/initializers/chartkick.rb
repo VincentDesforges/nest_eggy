7d13c3be4fe53305779ccf98e447fc6b25417bd0
Johann Goedhart <johann@goedhart.biz>
2018-03-07 18:15:03 +0000

    added spendings chart on breakdown page

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,4 +1,4 @@
 Chartkick.options = {
   height: '400px',
-  colors: ['green', 'black']
+  colors: ["#427D67", "#1E4235", "#045E3D", "#06A167", "#0EE393", "#80F2C8", "#CFCFCF", "#8C8C8C", "#454545", "#050505" ]
 }
