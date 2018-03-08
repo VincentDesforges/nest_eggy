2181d49f7b0827c0db6558bf7a213b4b441be704
Johann Goedhart <johann@goedhart.biz>
2018-03-08 14:44:03 +0000

    added column to catergories table and other stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307123620) do
+ActiveRecord::Schema.define(version: 20180308142715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180307123620) do
     t.integer "parent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "average"
   end
 
   create_table "transactions", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+uncategorized = Category.find_by(name: "Uncategorized")
+uncategorized.average =
