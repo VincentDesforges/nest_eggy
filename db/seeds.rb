552051e75e3395b4409b54c59a8c8328524977af
Vincent Desforges <desforges.vincent@gmail.com>
2018-03-14 16:04:35 +0000

    fixed the bug due to the missing id

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,8 +43,10 @@
 # puts "Done."
 
 Transaction.destroy_all
+PlanAccount.destroy_all
 Plan.destroy_all
 User.destroy_all
+Category.destroy_all
 BankAccount.destroy_all
 
 puts "Seed starts..."
@@ -75,21 +77,114 @@ user_id: user.id,
 api_account_id: 998,
   })
 
+puts "Categories"
+
+income_cat_1 = Category.create!({
+  api_category_id: 441890,
+  name: "Salaries",
+  parent_id: 166,
+  average: 510
+})
+
+income_cat_2 = Category.create!({
+  api_category_id: 78,
+  name: "Transfer",
+  parent_id: 303
+})
+
+spending_cat_1 = Category.create!({
+  api_category_id: 314,
+  name: "Rent",
+  parent_id: 2,
+  average: 33
+})
+
+spending_cat_2 = Category.create!({
+  api_category_id: 302,
+  name: "Taxes",
+  parent_id: 159,
+  average: 137
+})
+
+
+spending_cat_3 = Category.create!({
+  api_category_id: 273,
+  name: "Supermarkets / Groceries",
+  parent_id: 168,
+  average: 58
+})
+
+spending_cat_4 = Category.create!({
+  api_category_id: 226,
+  name: "Hobbies",
+  parent_id: 170,
+  average: 6
+})
+
+spending_cat_5 = Category.create!({
+  api_category_id: 87,
+  name: "Gas & Fuel",
+  parent_id: 165,
+  average: 20
+})
+
+
+spending_cat_6 = Category.create!({
+  api_category_id: 85,
+  name: "Withdrawals",
+  parent_id: 303,
+  average: 40
+})
+
+
+spending_cat_7 = Category.create!({
+  api_category_id: 83,
+  name: "Restaurants",
+  parent_id: 168,
+  average: 39
+})
+
+spending_cat_8 = Category.create!({
+  api_category_id: 171,
+  name: "Bills & Utilities",
+  average: 31
+})
+
+spending_cat_9 = Category.create!({
+  api_category_id: 180,
+  name: "Internet",
+  parent_id: 171,
+  average: 4
+})
+
+spending_cat_10 = Category.create!({
+  api_category_id: 276,
+  name: "Other spending",
+  parent_id: 160,
+  average: 83
+})
+
+spendings = [spending_cat_1, spending_cat_2 , spending_cat_3, spending_cat_4, spending_cat_5, spending_cat_6, spending_cat_7, spending_cat_8, spending_cat_9, spending_cat_10]
+incomes = [income_cat_1, income_cat_2]
+
 puts "Transactions..."
+
 
 api_id_checking = 0
 200.times do
   api_id_checking += 1
   Transaction.create!({
-    amount: rand(-1000..1000),
+    amount: rand(-150..-1),
     currency: "GBP",
     description: Faker::Job.field,
     date: Faker::Date.between(52.weeks.ago, Date.today),
     bank_account_id: checking_account.id,
     api_transaction_id: api_id_checking,
-    category_id: rand(1..141),
+    category: spendings.sample
 })
 end
+
+
 
 api_id_checking += 1
 Transaction.create!({
@@ -99,7 +194,7 @@ Transaction.create!({
   date: Date.today - 10.weeks,
   bank_account_id: checking_account.id,
   api_transaction_id: api_id_checking,
-  category_id: 36,
+  category: spending_cat_10,
 })
 
 month_counter = 13
@@ -114,7 +209,7 @@ api_id_saving = 300
     date: (Date.today - month_counter.months),
     bank_account_id: saving_account.id,
     api_transaction_id: api_id_saving,
-    category_id: 11,
+    category: income_cat_1,
 })
 end
 
@@ -126,13 +221,13 @@ Transaction.create!({
     date: (Date.today - 3.months),
     bank_account_id: saving_account.id,
     api_transaction_id: api_id_saving,
-    category_id: 11,
+    category: income_cat_1,
 })
 
 puts "Plan..."
 
 Plan.create!({
-  target_year: 2019,
+  target_year: 10,
   target_amount: 12000,
   weekly_savings_target: 200,
   user_id: user.id,
